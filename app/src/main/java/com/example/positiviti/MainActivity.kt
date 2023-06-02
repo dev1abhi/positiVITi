65b7c3699820b8e4b1b3dbc6324473faf6c39205
Kaustubh <kaustubhbiswas001@gmail.com>
2023-06-03 00:24:03 +0530

    1st chatbranch merge

--- app/src/main/java/com/example/positiviti/MainActivity.kt
+++ app/src/main/java/com/example/positiviti/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.positiviti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.demo)
+        setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().getReference()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId==R.id.logout){
+            //write the logic for logout
+            mAuth.signOut()
+            val intent = Intent(this@MainActivity,LogIn::class.java)
+            finish()
+            startActivity(intent)
+            return true
+        }
+        return true
+    }
+
 }
